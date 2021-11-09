49cc93abfc0835845ec98dacd4d5802ab23dd421
amnahasad <amnah_asad@hotmail.com>
2021-11-09 04:27:58 +0000

    Added the /set route

--- express_server.js
+++ express_server.js
@@ -19,6 +19,16 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/set", (req, res) => {
+    const a = 1;
+    res.send(`a = ${a}`);
+});
+   
+///fetch will give an error, since a is not defined in this route
+// app.get("/fetch", (req, res) => {
+//     res.send(`a = ${a}`);
+// });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
