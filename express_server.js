5685741542f1bc4ec7ad95d8b2a3f43b6c7cd825
amnah <amnah_asad@hotmail.com>
2021-11-10 04:20:46 +0000

    Updated express server to allow the shortURL and longUR: key-pair value to be saved in the urlDatabase

--- express_server.js
+++ express_server.js
@@ -37,6 +37,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[req.params.shortURL] = req.body.longURL;
 });
 
 app.get("/urls.json", (req, res) => {
