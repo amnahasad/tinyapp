199dc06b37ee5a1d2303dd8644ebb5d08c6268a1
Amnah Asad <amnah_asad@hotmail.com>
2021-11-13 22:46:39 +0000

    Added the mocha and chai package and test cases

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+const { mainModule } = require('process');
+
+const { emailLookup } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('emailLookup', function() {
+  it('should return a user with valid email', function() {
+    const user = emailLookup("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.equal(user.id, expectedUserID);
+  });
+  it('should return undefined if the email does not exist in the users database', function() {
+    const user = emailLookup("hamSandwich@example.com", testUsers)
+    assert.equal(user, undefined);
+  });
+});
