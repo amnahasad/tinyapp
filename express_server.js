ebf23b8b61d38088a508ac8502f9bc8fbeef3961
Amnah Asad <amnah_asad@hotmail.com>
2021-11-25 02:08:06 +0000

    Added comments

--- express_server.js
+++ express_server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const cookieSession = require('cookie-session');
 var cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
-const { emailLookup } = require("./helpers");
+const { emailLookup } = require("./helpers"); //the emailLookup helper function imported into this file.
 
 const app = express();
 app.use(cookieParser());
+
+//This is the cookie session that is being used within this file
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
@@ -16,6 +18,7 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+//The database that stores the users short and long url data
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
@@ -28,6 +31,7 @@ const urlDatabase = {
 };
 
 
+//This users object will store the users data for logging in
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -41,16 +45,17 @@ const users = {
   }
 };
 
+//This function generates a shortURL made up of random string of 6 characters
 function generateRandomString() {
   return Math.random().toString(36).substring(6);
 };
 
-
+//This is the GET request for the route '/'
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
+//This function takes in an id and checks to see if the id is a valid userID inside of the urlDatabase
 function urlsForUser(id) {
   let filteredUrls = {};
 
@@ -62,6 +67,8 @@ function urlsForUser(id) {
   return filteredUrls;
 }
 
+/* This is a GET request for the route to view the urls, if the user is not signed in, they are prompted to login. 
+They cannot see the urls unless they are logged in and unless the url is theirs. */
 app.get("/urls", (req, res) => {
   const userId = req.session.user_id;
   const user = users[userId];
@@ -75,6 +82,9 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
+/**This is a GET request of the route for creating a new url, if the user is not signed in, they are prompted to login. 
+They cannot create a new url unless they are logged in. */
 app.get("/urls/new", (req, res) => {
   const userId = req.session.user_id;
   const user = users[userId];
@@ -85,6 +95,8 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+/**This is a POST request for the route for viewing the urls, if the user is not signed in, they are prompted to login. 
+  They cannot view their urls unless they are logged in. */
 app.post("/urls", (req, res) => {
   const userId = req.session.user_id;
   const user = users[userId];
@@ -97,6 +109,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls`);
 });
 
+
+/**This is the GET request for the route for viewing the shortUrls, if the user is not signed in, they are prompted to login. 
+  They cannot view their shortURLs unless they are logged in. If the shortURL does not belong to them, then they cannot view it.
+  The user must be registered to create a short url and/or logged in to see their existing shortURLs. 
+  The user will otherwise be prompted with an error message. */
 app.get("/urls/:shortURL", (req, res) => {
   const userId = req.session.user_id;
   const user = users[userId];
@@ -117,6 +134,9 @@ app.get("/urls/:shortURL", (req, res) => {
   }
 });
 
+/*This is the GET request for the route for the shortURLs, if the shortURL for this user does not exist, their the user will
+ be prompted with an error message. The user must be logged in or registered inorder to view their 
+ shortURLs. Clicking on the shortURL will redirect the user to the longURL*/
 app.get("/u/:shortURL", (req, res) => {
   const shortUrl = req.params.shortURL;
   if (!urlDatabase[shortUrl]) {
@@ -130,6 +150,9 @@ app.get("/u/:shortURL", (req, res) => {
   }
 });
 
+/** This is a post request for deleting a shortUrl. The user must be logged in inorder to view and delete their
+ shortURLs. If the hsortUrL does not belong to the user, then the user cannot delete the shortURL. When the 
+ shortUrl is successfully deleted, the user will be redirected the the urls page.*/
 app.post("/urls/:shortURL/delete", (req, res) => {
   const userId = req.session.user_id;
   const user = users[userId];
@@ -150,6 +173,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 
+/* This POST request route is to view and update the shortUrls. A user can only view and edit their own shortUrl; 
+only when they are logged in. After the shortUrl is edited, they will be redirected back to the url page. If the 
+user does not exist and they are trying to reach a shortURL, they will be prompted with an error message and 
+redirected to the login page and they have to login before making any updates*/
 app.post("/urls/:shortURL", (req, res) => {
   const userId = req.session.user_id;
   const user = users[userId];
@@ -167,6 +194,10 @@ app.post("/urls/:shortURL", (req, res) => {
   }
 });
 
+
+/* This POST request route is for logging in. If the user enters an invalid email or 
+  password, they will be prompted with an error message. When the user successfully logs 
+  in, the user will be redirected to the urls page. */
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
   const user = emailLookup(email, users);
@@ -180,11 +211,15 @@ app.post("/login", (req, res) => {
   return res.redirect("/urls");
 });
 
+
+/* This is the POST request for the logout page.*/
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
 
+/* This is the GET request for the registration page. The user can enter a new email and 
+  password to register. */
 app.get("/register", (req, res) => {
   const userId = req.session.user_id;
   const user = users[userId];
@@ -192,6 +227,12 @@ app.get("/register", (req, res) => {
   res.render("urls_register", templateVars);
 });
 
+
+/* This POST request is for registration. The user can enter a new email and 
+    password to register. If the user enters an empty form for the email and/or 
+    password, the user will be prompted with an error message that the email or 
+    password is empter. If the user enters an existing email address to register, 
+    the user will be prompted with an error message that the email already exists.*/
 app.post("/register", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
@@ -204,6 +245,7 @@ app.post("/register", (req, res) => {
     return res.status(400).send("This email is already registered, please try a different email");
   }
 
+  //This generates a random hash string using bcrypt hashing to store the password safely, so the password is secure and no one can view it
   let id = generateRandomString();
   const hashedPassword = bcrypt.hashSync(password, 10);
   users[id] = {
@@ -215,6 +257,7 @@ app.post("/register", (req, res) => {
   res.redirect("/urls");
 });
 
+//This GET request route is for logging in, the user must have a valid userid.
 app.get("/login", (req, res) => {
   const userId = req.session.user_id;
   const user = users[userId];
@@ -222,19 +265,23 @@ app.get("/login", (req, res) => {
   res.render("urls_login", templateVars);
 });
 
+//This GET request route is a json request
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//This is a hello world GET request - testing
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//This is a GET request test for this route
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
 });
 
+//This listens for the port number and prints out a message to the terminal when the localhost connection is successful
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
--- helpers.js
+++ helpers.js
@@ -1,3 +1,7 @@
+/* This function takes in an email and usersDatabase and checks to see if 
+the email entered matches with the email in the database. If the email inputed matches 
+with the email in the database, then it will return the user from the database, else 
+the function will return undefined if the user does not exist. */
 function emailLookup(email, usersDatabase) {
     for (let key in usersDatabase) {
       if (email === usersDatabase[key].email) {
